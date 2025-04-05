ed6d0ee3e922483f7c754c89b661fce97954fa8c
BELMELLAT sami <belmellatsamy@gmail.com>
2025-04-05 13:11:34 +0200

    feat[#25] : add mutex for data race

--- src/Naimi_Trehel.c
+++ src/Naimi_Trehel.c
@@ -4,6 +4,8 @@ bool token;
 bool requesting;
 struct node_id father;
 struct node_id next;
+// mutex to manage data race 
+pthread_mutex_t mtx;
 
 static void send_request_to_father(struct node_id *requester) {
     size_t sz = sizeof(struct message) + sizeof(struct node_id);
@@ -21,6 +23,7 @@ static void send_token(struct node_id *dst) {
 }
 
 static void REQUEST_CS_handler(struct message *message) {
+    pthread_mutex_lock(&mtx);
     struct node_id *requester = (struct node_id *) (message + 1);
     if (node_equal(&father, &EMPTY_NODE)) {
         if (requesting) {
@@ -33,37 +36,49 @@ static void REQUEST_CS_handler(struct message *message) {
         send_request_to_father(requester);
     }
     node_copy(&father, requester);
+    pthread_mutex_unlock(&mtx);
 }
 
 static void GET_CS_handler(struct message *message) {
+    pthread_mutex_lock(&mtx);
     token = 1;
+    pthread_mutex_unlock(&mtx);
 }
 
 void request_CS() {
-    if (token == 1) return;
+    pthread_mutex_lock(&mtx);
     requesting = 1;
+    if (token == 1) return;
     if (!node_equal(&father, &EMPTY_NODE)) {
         send_request_to_father(&me);
     }
     struct message *msg = wait_message(GET_CS, NULL);
     free_message(msg);
+    pthread_mutex_unlock(&mtx);
 }
 
 void release_CS() {
+    pthread_mutex_lock(&mtx);
     requesting = 0;
     if (!node_equal(&next, &EMPTY_NODE)) {
         send_token(&next);
         token = 0;
         node_copy(&next, &EMPTY_NODE);
     }
+    pthread_mutex_unlock(&mtx);
 
 }
 
 void init_CS(struct node_id *father_init, bool token_init) {
     token = token_init;
     requesting = 0;
+    pthread_mutex_init(&mtx, NULL);
     node_copy(&father, father_init);
     node_copy(&next, &EMPTY_NODE);
     addHandler(REQUEST_CS, NULL, REQUEST_CS_handler);
     addHandler(GET_CS, NULL, GET_CS_handler);
+}
+
+void clear_CS() {
+    pthread_mutex_destroy(&mtx);
 }
