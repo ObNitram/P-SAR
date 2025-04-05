e48e58a2abd04142f878b54819845c02156a5c38
BELMELLAT sami <belmellatsamy@gmail.com>
2025-04-05 13:03:36 +0200

    fix[#25] : fix typos in logger macros

--- src/Naimi_Trehel.h
+++ src/Naimi_Trehel.h
@@ -15,8 +15,8 @@
 #ifdef NT_DEBUG
     #include "../utils/logger.h"
     #define LOG_NT(fmt, ...) log_info(fmt, ##__VA_ARGS__)
-    #define ENSURE_ERROR_NT(condition, fmt, ...) ensure_error(condition, fmr, ##__VA_ARGS__)
-    #define ENSURE_WARNING_NT(condition, fmt, ...) ensure_warning(condition, fmr, ##__VA_ARGS__)
+    #define ENSURE_ERROR_NT(condition, fmt, ...) ensure_error(condition, fmt, ##__VA_ARGS__)
+    #define ENSURE_WARNING_NT(condition, fmt, ...) ensure_warning(condition, fmt, ##__VA_ARGS__)
 #else
     #define LOG_NT(fmt, ...)
     #define ENSURE_ERROR_NT(condition, fmt, ...) 0
