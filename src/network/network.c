b4368fee9cd9adf82544909ed92e6e11159ec949
bobbywan-kenoby <leonoixdecoco@gmail.com>
2025-04-11 16:41:40 +0200

    fix[#34] patch get_server_ip return always 0.0.0.0
    Add interface to bind the server socket to a specific network interface and ensure that a node have only one ip adresse

--- src/network/network.c
+++ src/network/network.c
@@ -28,57 +28,43 @@ void * server_thread(void * arg)
 	pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
 
 	listen_sock = -1;
-	struct addrinfo hints, *res, *p;
-	int rv;
 	char listen_port[6]; // Listening port (as string)
+	const char *interface = (char *)arg;
 
-	snprintf(listen_port, sizeof(listen_port), "%d", server_port);
+	struct sockaddr_in serveraddr = { 0 };
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(server_port);
 
-	// Set up hints for getaddrinfo for a passive (server) socket.
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC; // Allow IPv4 or IPv6
-	hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
-	hints.ai_flags = AI_PASSIVE; // Use the local IP
-
-	if ((rv = getaddrinfo(NULL, listen_port, &hints, &res)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+	listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sock < 0) {
+		perror("socket");
 		return NULL;
 	}
 
-	// Loop through all results and bind to the first we can.
-	for (p = res; p != NULL; p = p->ai_next) {
-		listen_sock = socket(p->ai_family, p->ai_socktype,
-		                     p->ai_protocol);
-		if (listen_sock < 0) {
-			perror("socket");
-			continue;
-		}
-		// Enable address reuse.
-		int optval = 1;
-		if (setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR,
-		               &optval,
-		               sizeof(optval)) < 0) {
-			perror("setsockopt");
-			close(listen_sock);
-			continue;
-		}
-		if (bind(listen_sock, p->ai_addr, p->ai_addrlen) < 0) {
-			perror("bind");
-			close(listen_sock);
-			continue;
-		}
-		break; // Successfully bound.
-	}
-
-	if (p == NULL || listen_sock == -1) {
-		fprintf(
-			stderr,
-			"Failed to bind listening socket on port %s\n",
-			listen_port);
-		freeaddrinfo(res);
+	if (inet_pton(AF_INET, interface ? interface : "127.0.0.1",
+		      &serveraddr.sin_addr) <= 0) {
+		perror("inet_pton");
+		close(listen_sock);
 		return NULL;
 	}
-	freeaddrinfo(res);
+
+	char tmp_test[INET6_ADDRSTRLEN];
+	inet_ntop(AF_INET, &serveraddr.sin_addr, tmp_test, INET6_ADDRSTRLEN);
+
+	// Enable address reuse.
+	int optval = 1;
+	if (setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &optval,
+		       sizeof(optval)) < 0) {
+		perror("setsockopt");
+		close(listen_sock);
+		return NULL;
+	}
+	if (bind(listen_sock, (struct sockaddr *)&serveraddr,
+		 sizeof(serveraddr)) < 0) {
+		perror("bind");
+		close(listen_sock);
+		return NULL;
+	}
 
 	// Start listening for incoming connections.
 	if (listen(listen_sock, 5) < 0) {
@@ -166,7 +152,7 @@ void * server_thread(void * arg)
     return NULL;
 }
 
-void start_server(const int port)
+void start_server(const int port, const char* interface)
 {
 	LOG_NETWORK("Starting server on port %i", port);
 	server_port = port;
@@ -175,7 +161,7 @@ void start_server(const int port)
 	}
 
 	server_is_running = 1;
-	pthread_create(&server_thread_id, NULL, server_thread, NULL);
+	pthread_create(&server_thread_id, NULL, server_thread, (void *)interface);
 	// wait until the server is started
 	pthread_mutex_lock(&mutex);
 	pthread_cond_wait(&cond, &mutex);
@@ -210,29 +196,17 @@ int get_server_port()
 
 char * get_server_ip()
 {
+	struct sockaddr_in local_addr;
+	socklen_t addr_len = sizeof(local_addr);
+
+	if (getsockname(listen_sock, (struct sockaddr *)&local_addr,
+			&addr_len) == -1) {
+		perror("getsockname");
+		return NULL;
+	}
+
 	char *ip = malloc(INET6_ADDRSTRLEN);
-	if (ip == NULL) {
-		perror("malloc");
-		return NULL;
-	}
-
-	struct addrinfo hints, *res;
-	int rv;
-
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-
-	if ((rv = getaddrinfo("localhost", NULL, &hints, &res)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		free(ip);
-		return NULL;
-	}
-
-	struct sockaddr_in *addr = (struct sockaddr_in *)res->ai_addr;
-	inet_ntop(AF_INET, &addr->sin_addr, ip, INET6_ADDRSTRLEN);
-
-	freeaddrinfo(res);
+	inet_ntop(AF_INET, &local_addr.sin_addr, ip, INET6_ADDRSTRLEN);
 
 	return ip;
 }
--- src/network/network.h
+++ src/network/network.h
@@ -32,7 +32,8 @@
 /// @details Initializes the server and begins listening for incoming connections on the given port.
 ///          This function should be called before attempting to send or receive messages.
 /// @param port The port number on which the server will listen.
-void start_server(int port);
+/// @param interface The address on which the socket server will be bind, if NULL then 127.0.0.1 will be used.
+void start_server(int port, const char* interface);
 
 /// @brief Stops the running server.
 /// @details Gracefully stops the server by closing all connections and terminating the server thread.
