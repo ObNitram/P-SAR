8fd73898b8dc4711f96f3d2b70880875e8a78071
BELMELLAT sami <belmellatsamy@gmail.com>
2025-05-08 10:27:50 +0200

    big patch on send who is not thread safe

--- test/core/data_transfer_leave_test.cpp
+++ test/core/data_transfer_leave_test.cpp
@@ -40,21 +40,32 @@ TEST(data_transfer_leave, join_then_try_sync_a_page)
 {
 	init_logger(stdout);
 
+	sem_t *sem1, *sem2;
+
 	log_info("started test\n");
+	sem1 = sem_open("/sem1", O_CREAT, 0644, 0);
+	sem2 = sem_open("/sem2", O_CREAT, 0644, 0);
+
+	if (sem1 == SEM_FAILED || sem2 == SEM_FAILED) {
+		perror("sem_open");
+		exit(EXIT_FAILURE);
+	}
 
 	for (int i = 1; i <= 3; i++) {
 		pid_t pid = fork();
 		if (!pid) {
-			sleep(1);
+			sem_wait(sem1);
 			join_DSM(nodes[0].host, nodes[0].port, NULL,
 				 nodes[i].port);
 
 			log_info("me %d joined the DSM\n", i);
+			sem_post(sem2);
 
 			log_info("me %d try sync page 0\n", i);
 			sync_page(0);
 
-			sleep(1);
+			// wait until creator exited
+			sem_wait(sem1);
 
 			int eq = node_equal(&nodes[1], page_owners);
 			ASSERT_EQ(eq, 1);
@@ -72,13 +83,20 @@ TEST(data_transfer_leave, join_then_try_sync_a_page)
 
 	Init_DSM(SIZE_DSM, LOCALHOST, init_port);
 
-	// wait until every one joined
-	while (nb_nodees < 3) {
-	}
+	// inform every one that we are setup
+	for (int i = 0; i < 3; i++)
+		sem_post(sem1);
 
+	// wait until every one joined
+	for (int i = 0; i < 3; i++)
+		sem_wait(sem2);
 	log_info("init all joined\n");
 
 	leave_data_transfer(nodes[1]);
+
+	// inform that we leaved
+	for (int i = 0; i < 3; i++)
+		sem_post(sem1);
 
 	stop_server();
 	clean_core();
@@ -89,6 +107,15 @@ TEST(data_transfer_leave, join_then_try_sync_a_page)
 		wait(NULL);
 	nb_pages = 0;
 	nb_nodees = 0;
+	if (sem_close(sem1) == -1 || sem_close(sem2) == -1) {
+		perror("sem_close");
+		exit(EXIT_FAILURE);
+	}
+
+	if (sem_unlink("/sem1") == -1 || sem_unlink("/sem2") == -1) {
+		perror("sem_unlink");
+		exit(EXIT_FAILURE);
+	}
 }
 
 TEST(data_transfer_leave, leave_without_owning_a_page)
@@ -97,12 +124,13 @@ TEST(data_transfer_leave, leave_without_owning_a_page)
 
 	log_info("started test\n");
 
-	sem_t *sem1, *sem2;
+	sem_t *sem1, *sem2, *sem3;
 
 	sem1 = sem_open("/sem1", O_CREAT, 0644, 0);
 	sem2 = sem_open("/sem2", O_CREAT, 0644, 0);
+	sem3 = sem_open("/sem3", O_CREAT, 0644, 0);
 
-	if (sem1 == SEM_FAILED) {
+	if (sem1 == SEM_FAILED || sem2 == SEM_FAILED || sem3 == SEM_FAILED) {
 		perror("sem_open");
 		exit(EXIT_FAILURE);
 	}
@@ -110,13 +138,16 @@ TEST(data_transfer_leave, leave_without_owning_a_page)
 	for (int i = 1; i <= 3; i++) {
 		pid_t pid = fork();
 		if (!pid) {
-			sleep(1);
+			sem_wait(sem1);
+
 			join_DSM(nodes[0].host, nodes[0].port, NULL,
 				 nodes[i].port);
 			sem_post(sem2);
 			log_info("me %d joined the DSM\n", i);
 
-			sleep(1);
+			// wait until every one joined
+			sem_wait(sem1);
+
 			if (i != 1) {
 				log_info("me %d try sync page 0\n", i);
 				sync_page(0);
@@ -129,7 +160,7 @@ TEST(data_transfer_leave, leave_without_owning_a_page)
 
 			if (i != 1) {
 				log_info("me %d waiting for 1 to leave\n", i);
-				sem_wait(sem1);
+				sem_wait(sem3);
 				log_info("me %d woke up\n", i);
 
 				int eq = node_equal(&nodes[0], page_owners);
@@ -142,7 +173,7 @@ TEST(data_transfer_leave, leave_without_owning_a_page)
 			if (i == 1) {
 				log_info("me %d posting\n", i);
 				for (int i = 0; i < 3; i++)
-					sem_post(sem1);
+					sem_post(sem3);
 				log_info("me %d posted\n", i);
 			}
 
@@ -158,13 +189,22 @@ TEST(data_transfer_leave, leave_without_owning_a_page)
 
 	Init_DSM(SIZE_DSM, LOCALHOST, init_port);
 
+	// inform every one that we afre set up
+	for (int i = 0; i < 3; i++)
+		sem_post(sem1);
+
 	// wait until every one joined
 	for (int i = 0; i < 3; i++)
 		sem_wait(sem2);
 
+	// inform that every one joined
+	for (int i = 0; i < 3; i++)
+		sem_post(sem1);
+
 	log_info("init all joined\n");
 
-	sem_wait(sem1);
+	// wait for proc 1 to leave
+	sem_wait(sem3);
 	ASSERT_EQ(look_up_node(&nodes[1]), 0);
 
 	for (int i = 0; i < 2; i++)
@@ -174,12 +214,14 @@ TEST(data_transfer_leave, leave_without_owning_a_page)
 	free_nodes(&node_list);
 	munmap(dsm, nb_pages * PAGE_SIZE);
 
-	if (sem_close(sem1) == -1 || sem_close(sem2) == -1) {
+	if (sem_close(sem1) == -1 || sem_close(sem2) == -1 ||
+	    sem_close(sem3) == -1) {
 		perror("sem_close");
 		exit(EXIT_FAILURE);
 	}
 
-	if (sem_unlink("/sem1") == -1 || sem_unlink("/sem2") == -1) {
+	if (sem_unlink("/sem1") == -1 || sem_unlink("/sem2") == -1 ||
+	    sem_unlink("/sem3") == -1) {
 		perror("sem_unlink");
 		exit(EXIT_FAILURE);
 	}
